0790a84e9d83b8fafb3864b373cba51a950d526f
ahaddad98 <amine.haddadd@gmail.com>
2023-03-13 18:04:23 +0100

    add token validation but no need to add right now

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const {getUsers, getUsersById} = require('../controllers/user.contoller')
 const router = express.Router();
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = process.env.JWT_SECRET;
+function verifyToken(req, res, next) {
+    const token = req.headers.authorization;
+    if (!token) {
+      return res.status(401).send('Unauthorized request');
+    }
+    try {
+      const decoded = jwt.verify(token, secret);
+      req.user = decoded.user;
+      next();
+    } catch (err) {
+      res.status(400).send('Invalid token');
+    }
+  }
 router.get('/', getUsers);
 router.get('/:id', getUsersById);
 // router.post('/:id', loginView);
